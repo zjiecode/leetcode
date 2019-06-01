7d2f63eb11cdb940d34b121999277d20ef2b9bed
zjiecode <zjiecode@qq.com>
2019-06-01 14:10:53 +0800

    3. 无重复字符的最长子串-优化

--- src/main/java/com/zjiecode/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/zjiecode/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package com.zjiecode.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 说明：3. 无重复字符的最长子串
  * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
@@ -9,10 +12,37 @@ package com.zjiecode.leetcode;
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         LongestSubstringWithoutRepeatingCharacters longestSubstringWithoutRepeatingCharacters = new LongestSubstringWithoutRepeatingCharacters();
-        System.out.println(longestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("cabcd"));
+        System.out.println(longestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("abcbbcd"));
     }
 
+    /**
+     * 滑动窗口，从最左边开始，遇到重复字符，就把窗口的开始调整到重复的第一个字符右边，继续开始扫描
+     */
     public int lengthOfLongestSubstring(String s) {
+        if (s == null) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int len = s.length();
+        int max = 0;
+        int startIndex = 0;
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            Integer index = map.get(c);
+            if (index != null) {
+                startIndex = Math.max(startIndex, index + 1);
+            }
+            map.put(c, i);
+            max = Math.max(max, i - startIndex + 1);
+
+        }
+        return max;
+    }
+
+    /**
+     * 暴力求解
+     */
+    public int lengthOfLongestSubstring2(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
